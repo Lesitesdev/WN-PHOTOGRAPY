93e171ec4aee83ea5bcc92a25fe4117a8f96b25e
Lesitesdev <lesites.dev@gmail.com>
2025-01-10 16:11:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
-/* menu hamburguer */
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
 
-const menuHamburguer = document.querySelector('.menu-hamburguer');
-
-menuHamburguer.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-  const nav = document.querySelector('.nav-responsive');
-  menuHamburguer.classList.toggle('change');
-
-  if (menuHamburguer.classList.contains('change')) {
-    nav.style.display = 'block'; // Corrigido aqui
-  } else {
-    nav.style.display = 'none'; // Corrigido aqui
-  }
-}
-
-
-window.onload = function() {
-  const homeContent = document.querySelector('.home-conten');
-  homeContent.style.opacity = 1; // Torna o conteúdo visível após o carregamento
-};
-
-
-
-document.querySelectorAll('.faq-question').forEach(question => {
-  question.addEventListener('click', () => {
-      const answer = question.nextElementSibling; // Seleciona a resposta correspondente
-      answer.style.display = answer.style.display === 'block' ? 'none' : 'block'; // Alterna a visibilidade
-  });
-});
+hamburger.addEventListener("click", () => nav.classList.toggle("active"));
